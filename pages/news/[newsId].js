b80ad7bc7bd52423983d7dd73482f7c3cae67762
Jason Hubbard <jasonhubb@gmail.com>
2021-07-15 19:10:31 -0700

    adds useRouter to get url param

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  //Send newsId to api to fetch data
+
   return <h1>Detail Page</h1>;
 };
 
