cd378f0d469efcbeacfcec0b1904c574ab7111a4
Jason Hubbard <jasonhubb@gmail.com>
2021-07-15 19:19:51 -0700

    Adds dummy news list with Links

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 // domain/
+import Link from "next/link";
+import { Fragment } from "react";
 
 const HomePage = () => {
-  return <h1>The Home Page</h1>;
+  return (
+    <Fragment>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NewJS Is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something else...</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default HomePage;
